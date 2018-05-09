d77d84d7e3a5f30ecf832f7f57b098cabfb27a75
timrsfo <timrsfo@gmail.com>
2018-05-09 15:33:43 -0700

    added try finally

--- docker-selenium/docker-selenium.groovy
+++ docker-selenium/docker-selenium.groovy
@@ -25,38 +25,43 @@ node {
     def sel_chrome
     def sel_firefox
 
-	// In the preparation phase the three containers are run to parallelize the execution of chrome and firefox testing
-    stage('Preparation') {
-        sel_hub = docker.image('selenium/hub:3.4.0').run('-p 4444:4444 --name se-grid-hub')
-        sel_chrome = docker.image('selenium/node-chrome-debug:3.4.0').run('-p 5901:5900 --link se-grid-hub:hub')
-        sel_firefox = docker.image('selenium/node-firefox-debug:3.4.0').run('-p 5902:5900 --link se-grid-hub:hub')
-    }
-    
-	// Selenium test also run into containers. With inside the containers are automatically stop after execution
-    stage('Checkout') {
-      git 'https://github.com/timrsfo/selenium-example.git'
-      parallel (
-        firefox: {
-          docker.image('maven:3.3.9-jdk-8-alpine').inside { 
-            stage('Test firefox') {
-              sh 'mvn -B clean test -Dselenium.browser=firefox -Dsurefire.rerunFailingTestsCount=5 -Dsleep=0'
+    try {
+
+    // In the preparation phase the three containers are run to parallelize the execution of chrome and firefox testing
+      stage('Preparation') {
+          sel_hub = docker.image('selenium/hub:3.4.0').run('-p 4444:4444 --name se-grid-hub')
+          sel_chrome = docker.image('selenium/node-chrome-debug:3.4.0').run('-p 5901:5900 --link se-grid-hub:hub')
+          sel_firefox = docker.image('selenium/node-firefox-debug:3.4.0').run('-p 5902:5900 --link se-grid-hub:hub')
+      }
+      
+    // Selenium test also run into containers. With inside the containers are automatically stop after execution
+      stage('Checkout') {
+        git 'https://github.com/timrsfo/selenium-example.git'
+        parallel (
+          firefox: {
+            docker.image('maven:3.3.9-jdk-8-alpine').inside { 
+              stage('Test firefox') {
+                sh 'mvn -B clean test -Dselenium.browser=firefox -Dsurefire.rerunFailingTestsCount=5 -Dsleep=0'
+              }
+            }
+          },
+          chrome: {
+            docker.image('maven:3.3.9-jdk-8-alpine').inside { 
+              stage('Test chrome') {
+                sh 'mvn -B clean test -Dselenium.browser=chrome -Dsurefire.rerunFailingTestsCount=5 -Dsleep=0'
+              }
             }
           }
-        },
-        chrome: {
-          docker.image('maven:3.3.9-jdk-8-alpine').inside { 
-            stage('Test chrome') {
-              sh 'mvn -B clean test -Dselenium.browser=chrome -Dsurefire.rerunFailingTestsCount=5 -Dsleep=0'
-            }
-          }
-        }
-      )
+        )
+      }
     }
+    finally {
     
 	// Stop and remove the containers
-    stage('Clean up') {
+    //stage('Clean up') {
         sel_hub.stop()
         sel_firefox.stop()
         sel_chrome.stop()
+    //}
     }
 }
